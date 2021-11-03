1817ec45fa73c9f5600d132e64584995f27bb285
Kenny Park <kennyparc@gmail.com>
2021-11-03 22:56:44 +0000

    Test with HTTPie. #1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,45 @@
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
+const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
-
+const expressMiddleware = express.json();
 const app = express();
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+app.use(expressMiddleware);
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.post('/api/listings', (req, res, next) => {
+  const { userId, imageUrl, title, price, location, condition, description } = req.body;
+  const sql = `
+    insert into "listings"
+    ("userId", "imageUrl", "title", "price", "location", "condition", "description")
+    values ($1, $2, $3, $4, $5, $6, $7)
+    returning *;
+    `;
+  const values = [userId, imageUrl, title, price, location, condition, description];
+  if (!req.body.userId) {
+    throw new ClientError(400, 'Missing property: userId');
+  }
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows);
+    })
+    // .catch(err => next(err));
+    .catch(err => {
+      console.error(err);
+    });
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
