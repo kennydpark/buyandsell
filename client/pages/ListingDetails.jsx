56c23af43cd9daae318dd3fed3a63b77ebb95821
Kenny Park <kennyparc@gmail.com>
2021-11-11 23:50:25 +0000

    Confirm that the user interface looks natural on both mobile and desktop screens. #3

--- client/pages/ListingDetails.jsx
+++ client/pages/ListingDetails.jsx
@@ -24,7 +24,7 @@ export default class ListingDetails extends React.Component {
         <div className="row row-header justify-center">
           <h1 className="page-header-text">buyandsell</h1>
         </div>
-        <div className="row row-back-button">
+        <div className="row row-back-button justify-left">
           <a href="#browse-all"><i className="fas fa-angle-left back-icon dark-grey-color"></i></a>
         </div>
         <div className="details-container-full text-center">
