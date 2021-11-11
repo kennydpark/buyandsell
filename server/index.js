b0c9156b0f3097b52ebd25f0fe17aa2adb808608
Kenny Park <kennyparc@gmail.com>
2021-11-11 18:44:30 +0000

    Update server/index.js to add a GET request for a specific listing via listingId, returning all of the data. #3

--- server/index.js
+++ server/index.js
@@ -62,6 +62,27 @@ app.get('/api/listings', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/listings/:listingId', (req, res, next) => {
+  const listingId = parseInt(req.params.listingId, 10);
+  if (!listingId) {
+    throw new ClientError(400, 'listingId must be a positive integer');
+  }
+  const sql = `
+    select *
+    from "listings"
+    where "listingId" = $1
+  `;
+  const params = [listingId];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        throw new ClientError(404, `Cannot find listing with listingId ${listingId}`);
+      }
+      res.json(result.rows[0]);
+    })
+    .catch(err => next(err));
+});
+
 app.delete('/api/listings/:listingId', (req, res, next) => {
   const listingId = Number(req.params.listingId);
   if (!Number.isInteger(listingId) || listingId <= 0) {
