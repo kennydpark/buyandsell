fc2cf1272809aba4636e77ac15b51fa625ce19e7
Kenny Park <kennyparc@gmail.com>
2021-11-12 21:01:45 +0000

    Create a server/sendgrid.js file with the code provided that allows for sending an email. #4

--- server/index.js
+++ server/index.js
@@ -110,6 +110,27 @@ app.delete('/api/listings/:listingId', (req, res, next) => {
 
 app.use(errorMiddleware);
 
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+// javascript;
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com', // Change to your recipient
+//   from: 'test@example.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+// };
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent');
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
