c50d84cd73454f01539104f3cc9db3bd20cf3609
Kenny Park <kennyparc@gmail.com>
2021-11-29 18:46:32 +0000

    Added a link to the create new listing form in no-listings.jsx. #20

--- client/components/no-listings.jsx
+++ client/components/no-listings.jsx
@@ -11,6 +11,11 @@ export default class NoListings extends React.Component {
         <div className="form-container-full div-published-body text-center">
           <p className="dark-grey-color">When you start selling, your listings will appear here.</p>
         </div>
+        <div className="row row-create-new-listing-button justify-center">
+          <a href="#create-listing" className="create-new-listing-button">
+            <span><i className="fas fa-plus create-new-icon"></i></span> Create new listing
+          </a>
+        </div>
       </div>
     );
   }
