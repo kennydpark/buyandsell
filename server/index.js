f66f1da65e54460d9327cbde9620f6f1139f8277
Kenny Park <kennyparc@gmail.com>
2021-11-30 00:16:10 +0000

    Update index.js to refactor post method for submitting new listing for token authorization. #26

--- server/index.js
+++ server/index.js
@@ -109,37 +109,37 @@ app.post('/api/email', (req, res, next) => {
     });
 });
 
-app.post('/api/listings', uploadsMiddleware, (req, res, next) => {
-  const { userId, title, price, location, condition, description } = req.body;
-  if (!userId) {
-    throw new ClientError(400, 'Missing property: userId');
-  } else if (!req.file) {
-    throw new ClientError(400, 'Missing property: image');
-  } else if (!title) {
-    throw new ClientError(400, 'Missing property: title');
-  } else if (!price) {
-    throw new ClientError(400, 'Missing property: price');
-  } else if (!location) {
-    throw new ClientError(400, 'Missing property: location');
-  } else if (!condition) {
-    throw new ClientError(400, 'Missing property: condition');
-  } else if (!description) {
-    throw new ClientError(400, 'Missing property: description');
-  }
-  const url = req.file.location;
-  const sql = `
-    insert into "listings"
-    ("userId", "imageUrl", "title", "price", "location", "condition", "description")
-    values ($1, $2, $3, $4, $5, $6, $7)
-    returning *;
-    `;
-  const values = [userId, url, title, price, location, condition, description];
-  db.query(sql, values)
-    .then(result => {
-      res.status(201).json(result.rows);
-    })
-    .catch(err => next(err));
-});
+// app.post('/api/listings', uploadsMiddleware, (req, res, next) => {
+//   const { userId, title, price, location, condition, description } = req.body;
+//   if (!userId) {
+//     throw new ClientError(400, 'Missing property: userId');
+//   } else if (!req.file) {
+//     throw new ClientError(400, 'Missing property: image');
+//   } else if (!title) {
+//     throw new ClientError(400, 'Missing property: title');
+//   } else if (!price) {
+//     throw new ClientError(400, 'Missing property: price');
+//   } else if (!location) {
+//     throw new ClientError(400, 'Missing property: location');
+//   } else if (!condition) {
+//     throw new ClientError(400, 'Missing property: condition');
+//   } else if (!description) {
+//     throw new ClientError(400, 'Missing property: description');
+//   }
+//   const url = req.file.location;
+//   const sql = `
+//     insert into "listings"
+//     ("userId", "imageUrl", "title", "price", "location", "condition", "description")
+//     values ($1, $2, $3, $4, $5, $6, $7)
+//     returning *;
+//     `;
+//   const values = [userId, url, title, price, location, condition, description];
+//   db.query(sql, values)
+//     .then(result => {
+//       res.status(201).json(result.rows);
+//     })
+//     .catch(err => next(err));
+// });
 
 app.get('/api/listings', (req, res, next) => {
   const sql = `
@@ -223,6 +223,37 @@ app.delete('/api/listings/:listingId', (req, res, next) => {
 });
 
 app.use(authorizationMiddleware);
+
+app.post('/api/listings', uploadsMiddleware, (req, res, next) => {
+  const { userId } = req.user;
+  const { title, price, location, condition, description } = req.body;
+  if (!req.file) {
+    throw new ClientError(400, 'Missing property: image');
+  } else if (!title) {
+    throw new ClientError(400, 'Missing property: title');
+  } else if (!price) {
+    throw new ClientError(400, 'Missing property: price');
+  } else if (!location) {
+    throw new ClientError(400, 'Missing property: location');
+  } else if (!condition) {
+    throw new ClientError(400, 'Missing property: condition');
+  } else if (!description) {
+    throw new ClientError(400, 'Missing property: description');
+  }
+  const url = req.file.location;
+  const sql = `
+    insert into "listings"
+    ("userId", "imageUrl", "title", "price", "location", "condition", "description")
+    values ($1, $2, $3, $4, $5, $6, $7)
+    returning *;
+    `;
+  const values = [userId, url, title, price, location, condition, description];
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows);
+    })
+    .catch(err => next(err));
+});
 
 app.get('/api/user/listings', (req, res, next) => {
   const { userId } = req.user;
