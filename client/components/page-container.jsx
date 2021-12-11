9e6953f4b753154baa42bbdb61be323e6f996638
Kenny Park <kennyparc@gmail.com>
2021-12-11 00:45:37 +0000

    Renamed container class in page-container.jsx to differentiate between child container div elements.

--- client/components/page-container.jsx
+++ client/components/page-container.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageContainer({ children }) {
   return (
     <div>
-      <div className="container">
+      <div className="page-container">
         {children}
       </div>
     </div>
